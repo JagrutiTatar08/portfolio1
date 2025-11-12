889f4d8b5977f2c5de58cfa736e2fe04ae8d5442
Divyesh Gawad <divyeshgawad72@gmail.com>
2025-11-12 16:19:17 +0530

    Added Splash Component

--- src/app/splash/splash.component.ts
+++ src/app/splash/splash.component.ts
@@ -1,5 +1,5 @@
-import { Component, EventEmitter, Output, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Output, ElementRef, ViewChild, AfterViewInit, Inject, PLATFORM_ID } from '@angular/core';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-splash',
@@ -10,51 +10,46 @@ import { CommonModule } from '@angular/common';
 })
 export class SplashComponent implements AfterViewInit {
   @Output() splashEnded = new EventEmitter<void>();
-
   @ViewChild('welcomeText', { static: true }) welcomeText!: ElementRef<HTMLHeadingElement>;
 
   fullText = 'Welcome to My Portfolio';
   displayedText = '';
   currentIndex = 0;
-
-  progress = 0; // px
+  progress = 0;
   fadeOutSplash = false;
 
-  private charDelay = 145; // ms per character
+  private charDelay = 120;
+  private isBrowser: boolean;
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {
+    this.isBrowser = isPlatformBrowser(platformId);
+  }
 
   ngAfterViewInit() {
-    this.startTypewriter();
+    if (this.isBrowser) {
+      this.startTypewriter();
+    }
   }
 
   startTypewriter() {
     const totalChars = this.fullText.length;
-
     const typeNextChar = () => {
       if (this.currentIndex < totalChars) {
         this.currentIndex++;
         this.displayedText = this.fullText.substring(0, this.currentIndex);
-
-        // Update progress bar width
-        const fullWidth = this.getFullTextWidth();
-        this.progress = (this.currentIndex / totalChars) * fullWidth;
-
+        this.progress = (this.currentIndex / totalChars) * this.getFullTextWidth();
         setTimeout(typeNextChar, this.charDelay);
       } else {
-        // All text typed, ensure progress bar full
         this.progress = this.getFullTextWidth();
-
-        // Start fade out smoothly
         this.fadeOutSplash = true;
-
-        // Notify parent after fade-out duration
-        setTimeout(() => this.splashEnded.emit(), 0); // match fade-out CSS
+        setTimeout(() => this.splashEnded.emit(), 1000);
       }
     };
-
     typeNextChar();
   }
 
   getFullTextWidth(): number {
+    if (!this.isBrowser) return 200;
     const span = document.createElement('span');
     span.style.visibility = 'hidden';
     span.style.position = 'absolute';
